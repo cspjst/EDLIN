cb9857fe45cab72ed1530c55763683b07b3b1453
Hellorld! <jeremy.thornton@nhs.net>
2025-10-13 11:14:12 +0100

    Create edlin_parse.h

---
+++ src/EDLIN/edlin_parse.h
@@ -0,0 +1,47 @@
+#ifndef EDLIN_PARSE_H
+#define EDLIN_PARSE_H
+
+#include "edlin_types.h"
+#include <stdbool.h>
+
+// handler function signature for dispatch table
+typedef bool (*edlin_fn_command_t)(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_parse(edlin_cmd_t* cmd, edlin_file_t* file);
+
+// handler functions:
+bool edlin_fn_edit(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_help(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_end(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_quit(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_append(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_copy(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_delete(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_insert(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_list(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_move(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_page(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_write(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_transfer(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_replace(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_search(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_qreplace(edlin_cmd_t* cmd, edlin_file_t* file);
+
+bool edlin_fn_qsearch(edlin_cmd_t* cmd, edlin_file_t* file);
+
+#endif
