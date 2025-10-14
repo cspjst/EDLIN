fbf7497b48d8abcdb644cedc253875af80a2883f
Hellorld! <jeremy.thornton@nhs.net>
2025-10-14 11:20:41 +0100

    Update edlin_kbd.c

--- src/EDLIN/edlin_kbd.c
+++ src/EDLIN/edlin_kbd.c
@@ -3,22 +3,11 @@
 #include "../BIOS/bios_keyboard_scan_codes.h"
 #include <stdio.h>
 
-char edlin_kbd_key_char() {
-    bios_key_t key;
-    bios_wait_for_keystroke_and_read(&key);
-    return key.parts.ascii;
-}
-
-char edlin_kbd_scan_code()  {
-    bios_key_t key;
-    bios_wait_for_keystroke_and_read(&key);
-    return key.parts.scan;
-}
-
-bool edlin_kbd_yesno(const char* question) {
+bool edlin_yesno(const char* question) {
     printf("%s(Y/N)?", question);
     fflush(stdout);
-    bool result = (edlin_kbd_scan_code() == SCAN_Y);
-    if (!result) printf("\n");
-    return result;
+    bios_key_t key;
+    bios_wait_for_keystroke_and_read(&key);
+    if (key.parts.scan !=  SCAN_Y) printf("\n");
+    return key.parts.scan ==  SCAN_Y;
 }
