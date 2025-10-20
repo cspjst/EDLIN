c1ef77607a9c195ba120da8e645dde890015b71d
Hellorld! <jeremy.thornton@nhs.net>
2025-10-20 14:00:32 +0100

    Implement memory arena creation and management functions

---
+++ src/MEM/mem_arena.c
@@ -0,0 +1,29 @@
+#include "mem_arena.h"
+
+mem_arena_t* mem_arena_create(uint32_t paragraphs) {
+    mem_address_t addr = {0};
+    addr.segoff.segment = dos_allocate_memory_blocks(paragraphs);
+    if (! addr.segoff.segment) return NULL;
+  
+    mem_arena_t* arena = (mem_arena_t*)addr.ptr;
+
+    arena->segment = addr.segoff.segment;
+    arena->base = addr.ptr + sizeof(mem_arena_t);
+    arena->free_ptr = arena->base;
+    arena->end = arena->base + (paragraphs * 16);
+    
+    return arena;
+}
+
+void mem_arena_destroy(mem_arena_t* arena) {
+    if (arena) dos_free_allocated_memory_blocks(arena->segment);
+}
+
+void* mem_arena_alloc(mem_arena_t* arena, uint32_t size) {
+    char* ptr = arena->free_ptr;
+    if (ptr + size <= arena->end) {
+        arena->free_ptr += size;
+        return ptr;
+    }
+    return NULL;
+}
