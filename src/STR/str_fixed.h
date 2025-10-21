83e0cd7bb283436bd76cb65bbfa974caec5ce768
Hellorld! <jeremy.thornton@nhs.net>
2025-10-21 11:20:06 +0100

    Update str_fixed.h

--- src/STR/str_fixed.h
+++ src/STR/str_fixed.h
@@ -31,6 +31,8 @@ void str_print(str_fixed_t* str);
 
 void str_println(str_fixed_t* str);
 
+str_error_t str_itoa(int n, str_fixed_t* str, int base)
+
 #endif
 
 /*
@@ -49,5 +51,50 @@ void str_print(str_fixed_t* str) {
 void str_println(str_fixed_t* str) {
     str_fprint(1, str);
     dos_write_file(1, "\r\n", 2);
+}
+
+str_error_t str_itoa(int n, str_fixed_t* str, int base) {
+    if (!str) return STR_ERROR_NULL;
+    if (base < 2 || base > 36) return STR_ERROR_INVALID_BASE;
+    
+    char buffer[STR_FIXED_SIZE];
+    int i = STR_FIXED_SIZE - 1;
+    int is_negative = 0;
+    unsigned int un;
+    
+    if (n < 0 && base == 10) {
+        is_negative = 1;
+        un = -n;
+    } else {
+        un = (unsigned int)n;
+    }
+    
+    buffer[i--] = '\0';
+    do {
+        unsigned int digit = un % base;
+        buffer[i--] = (digit < 10) ? '0' + digit : 'a' + digit - 10;
+        un /= base;
+    } while (un > 0 && i >= 0);
+    
+    if (un > 0) return STR_ERROR_OVERFLOW;
+    
+    if (is_negative) {
+        if (i < 0) return STR_ERROR_OVERFLOW;
+        buffer[i--] = '-';
+    }
+    
+    // Copy without memcpy
+    char* src = &buffer[i + 1];
+    str_fixed_size_t size = 0;
+    while (size < STR_FIXED_SIZE - 1 && src[size] != '\0') {
+        str->text[size] = src[size];
+        size++;
+    }
+    
+    if (size >= STR_FIXED_SIZE) return STR_ERROR_OVERFLOW;
+    str->text[size] = '\0';
+    str->size = size;
+    
+    return STR_SUCCESS;
 }
 */
