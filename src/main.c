08b0b5db3a25d16f927dbf271905931984916645
Hellorld! <jeremy.thornton@nhs.net>
2025-10-21 11:35:54 +0100

    Implement tests for str_itoa function
    Add tests for str_itoa function with various cases.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,28 @@
 
 int main() {
-
+    str_fixed_t str;
+    
+    // Test successful conversion
+    str_print(&(str_fixed_t){"Testing itoa:", 13, 0});
+    str_itoa(12345, &str, 10);
+    str_println(&str);
+    
+    str_itoa(-6789, &str, 10); 
+    str_println(&str);
+    
+    str_itoa(255, &str, 16);
+    str_println(&str);
+    
+    // Test error cases
+    str_error_t err = str_itoa(99999, &str, 10);
+    if (err != STR_SUCCESS) {
+        str_print_error(err);
+    }
+    
+    err = str_itoa(123, NULL, 10);
+    if (err != STR_SUCCESS) {
+        str_print_error(err);
+    }
+    
+    return 0;
 }
