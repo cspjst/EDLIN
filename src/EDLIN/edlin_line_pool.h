08e50991dfabf06d761b9d45ca90643a75e7bf81
Hellorld! <jeremy.thornton@nhs.net>
2025-10-22 16:01:20 +0100

    Fix struct definition for edlin_line_buffer_t

---
+++ src/EDLIN/edlin_line_pool.h
@@ -0,0 +1,19 @@
+#ifndef EDLIN_LINE_BUFFER_H
+#define EDLIN_LINE_BUFFER_H
+
+#include "../STR/str_fixed.h"
+#include "edlin_types.h"
+
+typedef struct {
+    str_fixed_t* keys;
+    edlin_size_t size;
+    edlin_size_t capacity;
+} edlin_line_map;
+
+typedef struct {
+    str_fixed_t* lines;
+    edlin_size_t size;
+    edlin_size_t capacity;
+} edlin_line_pool_t
+
+#endif
