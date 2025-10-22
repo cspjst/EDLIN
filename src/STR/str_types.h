9f296ff8146463740684de33f836adbb1b858872
Hellorld! <jeremy.thornton@nhs.net>
2025-10-22 10:22:23 +0100

    Add include guards and define string types

--- src/STR/str_types.h
+++ src/STR/str_types.h
@@ -1,1 +1,19 @@
-#ifndef
+#ifndef STR_TYPES_H
+#define STR_TYPES_H
+
+typedef uint8_t str_size_t;
+
+typedef struct {
+    char text[STR_FIXED_SIZE];
+    str_size_t size;
+    uint8_t flags;
+} str_fixed_t;
+
+typedef enum {
+    STDIN = 0,
+    STDOUT,
+    STDERR,
+    PRN
+} str_devices_t;
+
+#endif
