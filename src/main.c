ed62697f12c9fa69b57b971bd0d792eeeb532ad1
Hellorld! <jeremy.thornton@nhs.net>
2025-10-23 16:44:31 +0100

    Replace CRLF variable with CRLF constant

--- src/main.c
+++ src/main.c
@@ -10,30 +10,30 @@ int main() {
     str_stdout(&CRLF);
     
     // Chain integer conversion
-    str_stdout(str_int(&str, -12345, 10), &CRLF);
+    str_stdout(str_int(&str, -12345, 10), CRLF);
     
     // Chain case conversion
-    str_stdout(str_to_upper(str_cstr(&str, "mixed case")), &CRLF);
+    str_stdout(str_to_upper(str_cstr(&str, "mixed case")), CRLF);
     
     // Chain trimming
-    str_stdout(str_trim(str_cstr(&str, "  spaces  ")), &CRLF);
+    str_stdout(str_trim(str_cstr(&str, "  spaces  ")), CRLF);
     
     // Chain reverse
-    str_stdout(str_reverse(str_cstr(&str, "reverse")), &CRLF);
+    str_stdout(str_reverse(str_cstr(&str, "reverse")), CRLF);
     
     // Multiple chains
-    str_stdout(str_int(str_cstr(&str, "Hex: "), 255, 16), &CRLF);
+    str_stdout(str_int(str_cstr(&str, "Hex: "), 255, 16), CRLF);
     
     // Copy chain
-    str_stdout(str_str(&str2, str_cstr(&str, "copied")), &CRLF);
+    str_stdout(str_str(&str2, str_cstr(&str, "copied")), CRLF);
     
     // Complex chain - trim, upper, reverse
-    str_stdout(str_reverse(str_to_upper(str_trim(str_cstr(&str, "  hello  ")))), &CRLF);
+    str_stdout(str_reverse(str_to_upper(str_trim(str_cstr(&str, "  hello  ")))), CRLF);
     
     // stdin with prompt chain
     str_fixed_t input, prompt;
     str_stdin_prompt(str_cstr(&prompt, "Enter text: "), &input);
-    str_stdout(str_cstr(&str, "You entered: "), &input, &CRLF);
+    str_stdout(str_cstr(&str, "You entered: "), &input, CRLF);
     
     return 0;
 }
