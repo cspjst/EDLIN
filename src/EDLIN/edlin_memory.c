f0609c9cd0440ad9c39512c8c6f4871f0017eae8
Hellorld! <jeremy.thornton@nhs.net>
2025-10-24 15:12:01 +0100

    Create edlin_memory.c

---
+++ src/EDLIN/edlin_memory.c
@@ -0,0 +1,20 @@
+#include "edlin_memory.h" 
+#include "../MEM/mem_arena.h"
+#include "../MEM/mem_tools.h"
+#include "../MEM/mem_constants.h"
+
+static mem_arena_t* edlin_arena;
+
+mem_size_bytes_t edlin_new_arena() {
+    mem_size_paragraphs_t total_free = mem_get_low_free();
+    edlin_arena = mem_new_arena(total_free); 
+    return total_free * MEM_PARAGRAPH_SIZE;
+}
+
+void edlin_free_arena() {
+    mem_free_arena(edlin_arena);
+}
+
+void* edlin_arena_allocate(mem_size_bytes_t bytes) {
+    return mem_arena_allocate(edlin, bytes);
+}
