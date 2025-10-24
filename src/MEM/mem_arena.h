1e81e6c89ca72733b1ee99b7ba03505cc5d3c0d9
Hellorld! <jeremy.thornton@nhs.net>
2025-10-24 15:11:05 +0100

    Update mem_arena.h

--- src/MEM/mem_arena.h
+++ src/MEM/mem_arena.h
@@ -15,6 +15,6 @@ mem_arena_t* mem_new_arena(mem_size_paragraphs_t paragraphs);
 
 void mem_free_arena(mem_arena_t* arena);
 
-void* mem_arena_alloc(mem_arena_t* arena, uint32_t bytes);
+void* mem_arena_alloc(mem_arena_t* arena, mem_size_bytes_t bytes);
 
 #endif
