229976d4044c969124a47910acec3aa45754ab31
Hellorld! <jeremy.thornton@nhs.net>
2025-11-05 11:57:58 +0000

    Update dos_file_tools.h

--- src/DOS/dos_file_tools.h
+++ src/DOS/dos_file_tools.h
@@ -8,7 +8,7 @@ uint8_t dos_file_exists(const char* path_name);
 
 uint8_t dos_file_is_eof(dos_file_handle_t fhandle);
 
-dos_file_size_t dos_file_size(const char* path_name);
+dos_file_size_t dos_file_size(dos_file_handle_t fhandle);
 
 char* dos_file_ext(const char* path_name);
 
