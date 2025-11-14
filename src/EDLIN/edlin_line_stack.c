2e3c076885d0d8c6e5bfdae92e5e990a2915432d
Hellorld! <jeremy.thornton@nhs.net>
2025-11-14 15:44:09 +0000

    Update edlin_line_stack.c

--- src/EDLIN/edlin_line_stack.c
+++ src/EDLIN/edlin_line_stack.c
@@ -1,13 +1,19 @@
 #include "edlin_line_stack.h"
 
-edlin_line_stack_t* edlin_new_line_stack(char* mem_ptr, edlin_size_t capacity) {
-
+edlin_line_stack_t* edlin_new_line_stack(void* mem_ptr, edlin_size_t capacity) {
+    edlin_line_stack_t* stack = (edlin_line_stack_t*)mem_start;
+    stack->base = (void**)((char*)mem_start + sizeof(mem_stack_t));
+    stack->top = stack->base;
+    stack->capacity = capacity;
+    return stack;
 }
 
 void edlin_line_stack_push(edlin_line_stack_t* stack, str_fixed_t* str) {
-
+    *(stack->top) = ptr;
+    stack->top++;
 }
 
 void* edlin_line_stack_pop(edlin_line_stack_t* stack) {
-
+    stack->top--;
+    return *(stack->top);
 }
