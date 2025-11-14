024ad5f118f9646bc931f1a9e7435ada5b3c4fc7
Hellorld! <jeremy.thornton@nhs.net>
2025-11-14 14:40:55 +0000

    Create edlin_line_stack.c

---
+++ src/EDLIN/edlin_line_stack.c
@@ -0,0 +1,13 @@
+#include "edlin_line_stack.h"
+
+edlin_line_stack_t* edlin_new_line_stack(char* mem_ptr, edlin_size_t capacity) {
+
+}
+
+void edlin_line_stack_push(edlin_line_stack_t* stack, str_fixed_t* str) {
+
+}
+
+void* edlin_line_stack_pop(edlin_line_stack_t* stack) {
+
+}
