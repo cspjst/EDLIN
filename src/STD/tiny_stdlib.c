ab0487aa44e9ec227edffd7356c8135601294982
Sauce Code... <jeremy.thornton@nhs.net>
2025-11-21 12:09:24 +0000

    Add memory allocation functions using DOS services

--- src/STD/tiny_stdlib.c
+++ src/STD/tiny_stdlib.c
@@ -1,11 +1,18 @@
 #include "tiny_stdlib.h"
+#include "tiny_assert.h"
+#include "../DOS/dos_memory_services.h"
 
 void* malloc(size_t size) {
-
+    dos_address_t addr = {0};
+    size = (uint16_t)((size + 15) >> 4); // convert to paragraphs (roundup) 
+    addr.segoff.segment = dos_allocate_memory_blocks(size);
+    return addr.ptr;
 }
 
 void free(void* p) {
-
+    dos_address_t addr = {0};
+    addr.ptr = p;
+    assert(dos_free_allocated_memory_blocks(addr.segoff.segment) == 0);
 }
 
 void* calloc(size_t n, size_t size) {
