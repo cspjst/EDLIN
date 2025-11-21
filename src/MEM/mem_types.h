95b14ad093e2d8f72a0833a8a38a2ff129a998d2
Sauce Code... <jeremy.thornton@nhs.net>
2025-11-21 10:22:53 +0000

    Remove mem_segoff_t and mem_address_t definitions
    Removed unused memory address types and structures.

--- src/MEM/mem_types.h
+++ src/MEM/mem_types.h
@@ -7,17 +7,4 @@ typedef uint16_t    mem_size_paragraphs_t;
 typedef uint32_t	mem_size_bytes_t;
 typedef int32_t		mem_diff_bytes_t;
 
-typedef struct {
-    uint16_t offset;
-    uint16_t segment;       // reverse order as Intel is little-endian
-} mem_segoff_t;
-
-typedef union {
-    char* ptr;
-    uint32_t memloc;
-    mem_segoff_t segoff;
-    uint16_t words[2];
-    uint8_t bytes[4];
-} mem_address_t;
-
 #endif
