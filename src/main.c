8296ea4f69c0888c479e5cf83af8593da4d0d321
Sauce Code... <jeremy.thornton@nhs.net>
2025-11-21 14:44:44 +0000

    Update includes and comment out test functions
    Comment out test_stdio function call and include test_stdlib.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 //#include "DOS/test_dos_files.h"
-#include "STD/test_stdio.h"
+//#include "STD/test_stdio.h"
+#include "STD/test_stdlib.h"
 //#include "MEM/test_arena.h"
 //#include "EDLIN/test_line_pool.h"
 //#include "EDLIN/test_line_sequence.h"
@@ -9,12 +10,11 @@
 
 int main() {
     //test_dos_files();
-    test_stdio();
+    //test_stdio();
+    test_stdlib();
     //test_mem_arena();
     //test_line_pool();
     //test_line_sequence();
     //test_file_buffer();
-
-    printf("sizeof size_t = %i", sizeof(size_t));
 
 }
