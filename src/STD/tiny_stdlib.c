5793d004a91624e44d5278108bb1020e0b87470f
Sauce Code... <jeremy.thornton@nhs.net>
2025-11-21 14:17:19 +0000

    Enhance malloc, free, and calloc functions
    Refactor memory allocation functions to handle errors and improve calloc implementation.

--- src/STD/tiny_stdlib.c
+++ src/STD/tiny_stdlib.c
@@ -5,16 +5,35 @@
 void* malloc(size_t size) {
     dos_address_t addr = {0};
     size = (uint16_t)((size + 15) >> 4); // convert to paragraphs (roundup) 
-    addr.segoff.segment = dos_allocate_memory_blocks(size);
-    return addr.ptr;
+    return (dos_allocate_memory_blocks(size, &addr.segoff.segment) == 0) ? addr.ptr : NULL;
 }
 
 void free(void* p) {
+    if (p == NULL) return;
     dos_address_t addr = {0};
     addr.ptr = p;
     assert(dos_free_allocated_memory_blocks(addr.segoff.segment) == 0);
 }
 
 void* calloc(size_t n, size_t size) {
+    dos_address_t addr = {0};
+    size = (uint16_t)(((size * n) + 15) >> 4); // size x n then convert to paragraphs (roundup) 
+    if(dos_allocate_memory_blocks(size, &addr.segoff.segment) == 0)) {
+        __asm {
+            .8086                   
+            pushf                 
+            
+            mov     ax, size         
+            shl     ax, 3
+            mov     cx, ax          ; CX = paragrpahs * 8 = number of WORDS needed
+            xor     ax, ax          
+            cld     
+            les     di, addr
+            rep     stosw           ; clear allocted memory
 
+            popf                    
+        }
+        return addr.ptr;
+    }
+    return NULL;
 }
