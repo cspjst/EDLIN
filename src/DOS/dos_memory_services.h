9be002b3aff765bfeac1c957cc812c1d3f27887f
Sauce Code... <jeremy.thornton@nhs.net>
2025-11-21 10:30:11 +0000

    Create dos_memory_services.h

---
+++ src/DOS/dos_memory_services.h
@@ -0,0 +1,21 @@
+#ifndef DOS_MEMORY_SERVICES_H
+#define DOS_MEMORY_SERVICES_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint16_t offset;
+    uint16_t segment;       // reverse order as Intel is little-endian
+} dos_segoff_t;
+
+typedef union {
+    char* ptr;
+    uint32_t memloc;
+    mem_segoff_t segoff;
+    uint16_t words[2];
+    uint8_t bytes[4];
+} dos_address_t;
+
+uint16_t dos_get_free_memory_paragraphs();
+
+#endif
