a6424de345535807b3b6d702b1a86a225c2afba7
Sauce Code... <jeremy.thornton@nhs.net>
2025-11-21 10:31:21 +0000

    Update memory functions to use DOS service

--- src/MEM/mem_tools.h
+++ src/MEM/mem_tools.h
@@ -4,18 +4,15 @@
 #include "mem_constants.h"
 #include "mem_types.h"
 #include "../BIOS/bios_memory_services.h"
+#include "../DOS/dos_memory_service.h"
 #include <stdint.h>
-
-mem_size_paragraphs_t mem_get_free_paragraphs();
 
 inline mem_size_bytes_t mem_get_startup_bytes() {
     return (mem_size_bytes_t)bios_get_startup_memory_kb() * MEM_1K_SIZE;
 }
 
 inline mem_size_bytes_t mem_get_free_bytes() {
-    return (mem_size_bytes_t)mem_get_free_paragraphs() * MEM_PARAGRAPH_SIZE;
+    return (mem_size_bytes_t)dos_get_free_memory_paragraphs() * MEM_PARAGRAPH_SIZE;
 }
-
-
 
 #endif
